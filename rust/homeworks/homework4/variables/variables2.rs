f6efed950e5cc34cbae60c887aac2a7f65939e6f
Lam Trinh <lam.trinh@global.t-bird.edu>
2023-07-31 22:57:37 +0900

    Finished homework 4

--- rust/homeworks/homework4/variables/variables2.rs
+++ rust/homeworks/homework4/variables/variables2.rs
@@ -1,13 +1,11 @@
 // variables2.rs
 // Make me compile! Execute the command `zustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
-    if x == 10 {
-        println!("x is ten!");
-    } else {
-        println!("x is not ten!");
-    }
+  let x: i32 = 10;
+  if x == 10 {
+      println!("x is ten!");
+  } else {
+      println!("x is not ten!");
+  }
 }
